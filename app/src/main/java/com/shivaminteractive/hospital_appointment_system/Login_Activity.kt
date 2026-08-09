a9cdd93edb4bf1964a0a3d08649ea01fbdc352cc
Shivam <shivamprajapati2006@gmail.com>
2026-08-09 18:24:21 +0530

    Login_UI_Comp

--- app/src/main/java/com/shivaminteractive/hospital_appointment_system/Login_Activity.kt
+++ app/src/main/java/com/shivaminteractive/hospital_appointment_system/Login_Activity.kt
@@ -1,5 +1,6 @@
 package com.shivaminteractive.hospital_appointment_system
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -12,26 +13,21 @@ import androidx.core.view.WindowInsetsCompat
 
 class Login_Activity : AppCompatActivity() {
 
-    lateinit var email: EditText
-    lateinit var password: EditText
-    lateinit var button:Button
+    lateinit var email_input: EditText
+    lateinit var pass_input: EditText
+    lateinit var login:Button
+    lateinit var register:Button
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
 
 
-        email=findViewById(R.id.emailAddress)
-        password=findViewById(R.id.Password)
-        button=findViewById(R.id.loginbtn)
+        email_input=findViewById(R.id.email_input)
+        pass_input=findViewById(R.id.pass_input)
 
-
-        button.setOnClickListener {
-
-            Log.d("Login_Info","Email : ${email.text.toString()}  Password : ${password.text.toString()}" )
-
-            Toast.makeText(this,"Details Shared",Toast.LENGTH_SHORT).show()
-
-        }
+        login=findViewById(R.id.btLogin)
+        register=findViewById(R.id.btnRegistation)
     }
 }
